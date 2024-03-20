321c38ef7a026f3a80e64a7e69667e117c246ea6
Mukund Katpatal <mukund.katpatal@aerdata.com>
2024-03-20 12:02:31 +0100

    Fix the image paths

--- dist/profile/server/main.server.mjs
+++ dist/profile/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2MA6I5EG.mjs";import"./chunk-JPGLWAV2.mjs";import"./chunk-24BL2CO5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-Z5RGVMB2.mjs";import"./chunk-JPGLWAV2.mjs";import"./chunk-24BL2CO5.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
